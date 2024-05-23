377106cafeaa1784c4a952de77165d4a649ef9e0
HiddenGrizzly <153844656+BrownyGrizzly@users.noreply.github.com>
2024-05-23 19:26:05 +0700

    Reapply "CRUD for Movie"
    This reverts commit 7bdc1b5c8765369af03ccacbc75a4f452d7b2ab4.

---
+++ moviemingle/src/main/java/com/example/moviemingle/services/directors/DirectorService.java
@@ -0,0 +1,18 @@
+package com.example.moviemingle.services.directors;
+
+import com.example.moviemingle.entities.Director;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DirectorService {
+    public Page<Director> findAllDirectors(int page, int size);
+
+    public Director findDirectorById(Long id);
+
+    public Director saveDirector(String directorName);
+
+    public void deleteDirector(Long id);
+}
