6b3b16c27a347d3878106d76aa1ffb338f427ebb
hoangvhth2211016 <hoangvhth2211016@fpt.edu.vn>
2024-05-29 13:54:04 +0700

    pull from main

---
+++ moviemingle-app/src/app/pages/user-admin/user-admin.component.ts
@@ -0,0 +1,44 @@
+import { PageReq } from '../../models/pages/PageReq';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
+import { Component, OnInit } from '@angular/core';
+import { UserRes } from 'src/app/models/users/UserRes';
+import { PageInfo } from 'src/app/models/pages/PageInfo';
+
+@Component({
+  selector: 'app-user-admin',
+  templateUrl: './user-admin.component.html',
+  styleUrls: ['./user-admin.component.css']
+})
+export class UserAdminComponent implements OnInit {
+
+  users: UserRes[] = [];
+
+  pageReq: PageReq | null = null;
+
+  pageInfo!: PageInfo;
+
+  constructor(
+    private userService: UserService,
+    private ActivatedRoute: ActivatedRoute,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.ActivatedRoute.queryParams.subscribe(params => {
+      if (params['page'] && params['size']) {
+        this.pageReq = {
+          page: Number(params['page']),
+          size: Number(params['size'])
+        }
+      }
+      this.userService.getAll(this.pageReq).subscribe(res => {
+        this.pageInfo = new PageInfo(res);
+        this.users = res.content;
+        console.log(res);
+      });
+    })
+  }
+
+}
---
+++ moviemingle-app/src/app/pages/user-movie/user-movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-movie',
+  templateUrl: './user-movie.component.html',
+  styleUrls: ['./user-movie.component.css']
+})
+export class UserMovieComponent {
+
+}
